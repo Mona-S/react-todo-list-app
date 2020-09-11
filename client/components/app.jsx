b69b59ea32c45e433e6e32908fad941e89411a86
Mona-S <forward2cm@gmail.com>
2020-09-11 12:48:02 +0000

    added onchange on input and set the value to inputText

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 
 function App(){
+
+    const [inputText, setInputText] = useState("");
+
     return(
         <div className="container">
             <div className="heading">
                 <h1>To-Do List</h1>
             </div>
             <div className="form">
-                <input type="text" />
+                <input onChange={handleChange} type="text" value={inputText}/>
                 <button><span>Add</span></button>
             </div>
             <div>
